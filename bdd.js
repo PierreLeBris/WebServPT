c6a571cb9806408f9246d211000a1d6e2a302524
Thomas FERREIRA <thomas.ferreira80@gmail.com>
2023-03-09 16:59:38 +0100

    Update bdd.js

--- bdd.js
+++ bdd.js
@@ -5,9 +5,21 @@ var BaseDeDonnees = {};
 
 var server = http.createServer(function(req, res) {
     var path = req.url.split('?')[0];
-    if(!path || path =='/createbdd'){
-
-        if (req.method == "PUT") {
+    if (!path || path =='/') {
+        if (req.method === 'GET') {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            if(BaseDeDonnees){
+                console.log(BaseDeDonnees);
+                res.end(JSON.stringify(BaseDeDonnees));
+            }
+            else{
+                res.writeHead(404, {'Content-type': 'text/plain'});
+                res.end("Not Found");
+            } 
+        }
+    }
+    else if(!path || path =='/createbdd'){
+        if (req.method == "POST") {
             var body = '';
             req.on('data', function(data){
                 body += data.toString();
@@ -17,7 +29,6 @@ var server = http.createServer(function(req, res) {
                 if(!BaseDeDonnees[body]){
                     BaseDeDonnees[body] = {};
                 }
-                //BaseDeDonnees[body].push(body);
                 console.log(BaseDeDonnees);
                 res.end('{Database created}');
             });  
@@ -28,14 +39,38 @@ var server = http.createServer(function(req, res) {
         } 
     }
     else if (!path || path!='/createbdd' || path!='/') {
-        if(req.method==="GET"){
-            res.writeHead(200, {'Content-type': 'application/json'});
-            if(!BaseDeDonnees[path]){
-                console.log(BaseDeDonnees);
-                res.end(JSON.stringify([]));
-            } 
+        pathBdd = path.split('/')[1];
+        console.log(pathBdd);
+        if(req.method ==="GET"){
+            //res.writeHead(200, {'Content-type': 'application/json'});
+            if(BaseDeDonnees[pathBdd]){
+                res.end(JSON.stringify(BaseDeDonnees[pathBdd]));
+            }
+            else{
+                res.writeHead(404, {'Content-type': 'text/plain'});
+                res.end("Not Found");
+            }
         }
-    }  
+        if (req.method === "POST" && BaseDeDonnees[pathBdd]) {
+            var body = '';
+            res.writeHead(200, {'Content-type': 'application/json'});
+            req.on('data', function(data){
+                body += data.toString();
+            });
+            req.on('end', function(){
+                console.log(BaseDeDonnees[pathBdd]);
+                if(!BaseDeDonnees[pathBdd][body]){
+                    BaseDeDonnees[pathBdd][body] = {};
+                }
+                console.log(BaseDeDonnees);
+                res.end('{Table created}');
+            });
+        }
+        else{
+            res.writeHead(404, {'Content-type': 'text/plain'});
+            res.end("Not Found"); 
+        }
+    }
 })
 
 server.listen(8000);
